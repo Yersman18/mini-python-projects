10ac9626ef0e5f72051d738bdae2aee7d9608c45
Yersman18 <yersmangarzon@gmail.com>
2026-01-28 23:16:09 -0500

    haciendo mini proyectos

---
+++ menu_interactivo/menu.py
@@ -0,0 +1,24 @@
+# creando un menu interactivo
+#Menu de comidas 
+
+
+while True:
+    print("Menu de comidas rapidas \n")
+    print("1. Hamburguesas")
+    print("2. Pizzas")
+    print("3. Perros calientes")
+    print("4. Mazorcadas")
+    print("5. Salir \n")
+    try: 
+        opcion = int(input("Escribe una opcion: "))
+    except ValueError:
+        print("escoge una opcion valida")
+    
+    if opcion == 1:
+        print('Este es el menu de hamburguesas')
+        print('1. todo terreno')
+        elif 
+    break    
+
+
+        
