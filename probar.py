de33cecb5e38295b9c34b014575d624d264602fc
Yersman18 <yersmangarzon@gmail.com>
2026-02-05 16:59:54 -0500

    avanzando en el login

--- probar.py
+++ probar.py
@@ -1,4 +1,9 @@
-usuario = dict.fromkeys(["nombre", "contraseña"])
+lista = ["NOMRE", "edad"]
+buscar = input("Escribe tu nombre")
 
-print(usuario )
+if buscar in lista:
+    print("Si esta")
+else:
+    print("noo esta")
 
+# sistema que nos ayuda a buscar la informacion de las personas
--- sistemas de usuarios/registro.py
+++ sistemas de usuarios/registro.py
@@ -2,20 +2,32 @@
 # Hacer que un dicccinario de un usuario quede en una lista
 
 usuarios = []
-
+persona  = dict.fromkeys(["nombre", "contraseña"])
+opcion = 0
 
 while True:
-    print("LOGIN")
-    print("")
-    # Creando un diccionario vacio solo con las claves
-    persona  = dict.fromkeys(["nombre", "contraseña"])
-    # agregando datos a las claves
-    persona["nombre"] = input("Escribe tu nombre: ")
-    persona["contraseña"] = input("Escribe tu pin: ")
-    # agregando el diccionario a una lista
-    usuarios.append(persona)
-    print("Registrado correctamente! Ya puedes iniciar sesion")
+    print("LOGIN \n")
+    print("1. Registrarse")
+    print("2. Iniciar sesion ")
+    print("3. Salir \n")
     while True:
-        if persona["nombre"] == "" or persona["contraseña"] == "":
-            print('Todos los campos son obligatorios \nRegistrate nuevamente!')
-            break
+        try:
+            opcion = int(input("Ingresa una opcion: "))
+        except ValueError:
+            print("Ingresa una opccion valida")
+        break
+    if opcion  == 1: 
+            # Creando un diccionario vacio solo con las claves
+            # agregando datos a las claves
+            persona["nombre"] = input("Escribe tu nombre: ")
+            persona["contraseña"] = input("Escribe tu pin: ")
+            if persona["nombre"] == "" or persona["contraseña"] == "":
+                print("Todos los campos son obligatorios\n")
+            else:
+                usuarios.append(persona)
+                print("Agregado correctamente")
+                break
+    
+print(usuarios)
+
+print(type(usuarios))
