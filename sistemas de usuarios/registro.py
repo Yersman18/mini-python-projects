787357baab9c00ab3b6f5ea7b3ad028fe568014d
Yersman18 <yersmangarzon@gmail.com>
2026-02-05 23:22:43 -0500

    apredieno nuevo concepto

--- sistemas de usuarios/registro.py
+++ sistemas de usuarios/registro.py
@@ -1,33 +1,33 @@
 
 # Hacer que un dicccinario de un usuario quede en una lista
-
 usuarios = []
-persona  = dict.fromkeys(["nombre", "contraseña"])
-opcion = 0
+
 
 while True:
     print("LOGIN \n")
     print("1. Registrarse")
     print("2. Iniciar sesion ")
     print("3. Salir \n")
-    while True:
-        try:
-            opcion = int(input("Ingresa una opcion: "))
-        except ValueError:
-            print("Ingresa una opccion valida")
-        break
-    if opcion  == 1: 
-            # Creando un diccionario vacio solo con las claves
-            # agregando datos a las claves
-            persona["nombre"] = input("Escribe tu nombre: ")
-            persona["contraseña"] = input("Escribe tu pin: ")
-            if persona["nombre"] == "" or persona["contraseña"] == "":
-                print("Todos los campos son obligatorios\n")
-            else:
-                usuarios.append(persona)
-                print("Agregado correctamente")
-                break
-    
-print(usuarios)
+    try:
+        opcion = int(input("Ingresa una opcion: "))
+    except ValueError:
+        print("Ingresa una opccion valida")
 
-print(type(usuarios))
+    if opcion  == 1:
+        persona = {}     
+        persona["nombre"] = input("Escribe tu nombre: ")
+        persona["contraseña"] = input("Escribe tu pin: ")
+        usuarios.append(persona)
+
+    elif opcion == 2:
+        print(usuarios)
+    
+            
+        
+    
+
+          
+                    
+    
+
+
