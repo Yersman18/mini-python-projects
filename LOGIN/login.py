e0b1cc1e55eec60cce69d5de7323fea2d18aafd4
Yersman18 <yersmangarzon@gmail.com>
2026-02-12 17:18:38 -0500

    Avanzando tema

--- LOGIN/login.py
+++ LOGIN/login.py
@@ -17,24 +17,33 @@ while True:
             print("Escribe los siguientes datos: ")
             usuario["nombre"] = input("Nombre: ")
             usuario["password"] = input("Escribe un pin: ")
+            if usuario["nombre"] == "" or usuario["password"] == "":
+                print("No se reciben espacios en blanco")
+                break
             for i in usuarios:
                 if usuario["nombre"] == i["nombre"]:
                     existe = True
-                           
             if existe:
                 print("este nombre ya esta creado agrega otro")
             else:
                 print("Se agrego correctamene!")
                 usuarios.append(usuario)
+                print(usuarios)
             break
         elif opcion == 2:
-            print("Escribe los datos que ingresaste para iniciar sesion")
-            usuario["nombre"] = input("Nombre: ")
-            usuario["password"] = input("Escribe un pin: ")
-            for i in usuarios:
-                if usuario["nombre"] == i["nombre"] and  usuario["password"] == i["password"]:
+            print("Escribe los datos que registraste para iniciar sesion")
+            try:
+                nombre_ingresado = input("Nombre: ")
+            except NameError:
+                print("Este nombre no se encuentra registrado en el sistema registrese primero") 
+                break
+            contraseña_ingresada = input("Contraseña: ")
+            print("usuarios guardados", usuarios)
+            for i in usuarios:  
+                if nombre_ingresado == i["nombre"] and contraseña_ingresada == i["password"]:
                     print("Inicio de sesion correcto")
-                    print(f"Bienvenido al mundo de la proramacion {usuario['nombre']}")
+                    print(f"Bienvenido al mundo de la proramacion {i['nombre']}")
+                    print("0. Salir")
                     break
             else:
                 print("Credenciales incorrectas")
